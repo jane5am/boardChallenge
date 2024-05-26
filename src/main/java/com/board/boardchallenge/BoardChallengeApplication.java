39ecc2782b09ce6ebed96c3bc041629a9303d21e
hyejin <hyejda00@naver.com>
2024-05-26 15:56:29 +0900

    2024.05.26 commit 테스트

---
+++ src/main/java/com/board/boardchallenge/BoardChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.board.boardchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/board/boardchallenge/BoardChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.boardchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
